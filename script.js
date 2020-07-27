782c048ded6a6c4207b8b373a58040aded085dd0
soeun <70000city@gmail.com>
2020-07-27 23:54:37 +0900

    2

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// console.log(document.querySelector(".box").style.width);
+let width = 405;
+let count = 3;
+
+let carousel = document.querySelector(".carousel");
+let list = carousel.querySelector(".box>ul");
+let listElems = list.querySelectorAll("li");
+
+let position = 0;
+
+carousel.querySelector(".button>ul>li").onclick = function () {
+  position += width * count;
+  position = Math.max(position, width * (listitems.length - count));
+  list.style.marginLeft = position + "px";
+};
+
+carousel.querySelector(".button>ul").children[2].onclick = function () {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+};
